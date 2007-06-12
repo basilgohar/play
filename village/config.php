c3ad3c3b373cd3979ac1754ecfeeff605e3aff8b
(no author) <(no author)@028c0714-f59e-4c53-96ac-4b4ac9e3dc78>
2007-06-12 15:35:39 +0000

    moved tables creation sql into config.php, which is now used by both create-tables.php & destroy-tables.php;
    git-svn-id: http://svn.hidayahonline.org/repos/play@34 028c0714-f59e-4c53-96ac-4b4ac9e3dc78

--- village/config.php
+++ village/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('VILLAGE_POPULATION', 10000000);
+define('VILLAGE_POPULATION', 100000);
 define('VILLAGE_DISPLAY_LIMIT', 100);
 define('VILLAGE_CONTROLLER_PATH', '/home/basil.gohar/public_html/play/village/controllers');
 define('VILLAGE_SPOUSE_MAX_MALE', 4);
@@ -16,3 +16,101 @@ require_once 'lib/tables.php';
 require_once 'lib/person.php';
 require_once 'lib/marriage.php';
 require_once 'lib/display.php';
+
+$tables_sql = array();
+
+//  Ensure all tables SQL have IF NOT EXISTS!
+/*
+$tables_sql['Names'] = "CREATE TABLE IF NOT EXISTS `Names` (
+  `id` mediumint(5) unsigned NOT NULL auto_increment,
+  `value` varchar(13) collate utf8_unicode_ci NOT NULL,
+  `type` enum('female','last','male') collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
+
+$tables_sql['People'] = "CREATE TABLE IF NOT EXISTS `People` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `name_first_id` mediumint(5) unsigned NOT NULL,
+  `name_last_id` mediumint(5) unsigned NOT NULL,
+  `gender` enum('female','male') collate utf8_unicode_ci NOT NULL,
+  `date_birth` datetime NOT NULL,
+  `date_death` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `name_first_id` (`name_first_id`),
+  KEY `name_last_id` (`name_last_id`),
+  CONSTRAINT `name_first_id_fk` FOREIGN KEY (`name_first_id`) REFERENCES `Names` (`id`),
+  CONSTRAINT `name_last_id_fk` FOREIGN KEY (`name_last_id`) REFERENCES `Names` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
+
+$tables_sql['Families'] = "CREATE TABLE IF NOT EXISTS `Families` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `person_id` int(10) unsigned NOT NULL,
+  `mother_id` int(10) unsigned NOT NULL,
+  `father_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `person_id` (`person_id`),
+  KEY `mother_id` (`mother_id`),
+  KEY `father_id` (`father_id`),
+  CONSTRAINT `person_id_fk` FOREIGN KEY (`person_id`) REFERENCES `People` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `mother_id_fk` FOREIGN KEY (`mother_id`) REFERENCES `People` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `father_id_fk` FOREIGN KEY (`father_id`) REFERENCES `People` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+";
+
+$tables_sql['Marriages'] = "CREATE TABLE IF NOT EXISTS `Marriages` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `husband_id` int(10) unsigned NOT NULL,
+  `wife_id` int(10) unsigned NOT NULL,
+  `date_married` datetime NOT NULL,
+  `date_divorced` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `husband_id` (`husband_id`),
+  KEY `wife_id` (`wife_id`),
+  CONSTRAINT `husband_id_fk` FOREIGN KEY (`husband_id`) REFERENCES `People` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `wife_id_fk` FOREIGN KEY (`wife_id`) REFERENCES `People` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
+*/
+///*
+$tables_sql['Names'] = "CREATE TABLE IF NOT EXISTS `Names` (
+  `id` mediumint(5) unsigned NOT NULL auto_increment,
+  `value` varchar(13) collate utf8_unicode_ci NOT NULL,
+  `type` enum('female','last','male') collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
+
+$tables_sql['People'] = "CREATE TABLE IF NOT EXISTS `People` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `name_first_id` mediumint(5) unsigned NOT NULL,
+  `name_last_id` mediumint(5) unsigned NOT NULL,
+  `gender` enum('female','male') collate utf8_unicode_ci NOT NULL,
+  `date_birth` datetime NOT NULL,
+  `date_death` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `name_first_id` (`name_first_id`),
+  KEY `name_last_id` (`name_last_id`),
+  KEY `gender` (`gender`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
+
+$tables_sql['Families'] = "CREATE TABLE IF NOT EXISTS `Families` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `person_id` int(10) unsigned NOT NULL,
+  `mother_id` int(10) unsigned NOT NULL,
+  `father_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `person_id` (`person_id`),
+  KEY `mother_id` (`mother_id`),
+  KEY `father_id` (`father_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+";
+
+$tables_sql['Marriages'] = "CREATE TABLE IF NOT EXISTS `Marriages` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `husband_id` int(10) unsigned NOT NULL,
+  `wife_id` int(10) unsigned NOT NULL,
+  `date_married` datetime NOT NULL,
+  `date_divorced` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `husband_id` (`husband_id`),
+  KEY `wife_id` (`wife_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
+//*/
--- village/destroy-tables.php
+++ village/destroy-tables.php
@@ -2,9 +2,6 @@
 
 require_once 'config.php';
 
-$table_names = array('Marriages', 'Families', 'People', 'Names');
-
-foreach ($table_names as $table_name) {
-    $sql = "DROP TABLE IF EXISTS `" . $table_name . "`";
-    $db->query($sql);
+foreach ($tables_sql as $table_name => $table_sql) {
+    $db->query("DROP TABLE IF EXISTS `$table_name`");
 }
