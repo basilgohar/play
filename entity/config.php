b279a874e2ffa38377d216bffb0e8f4d9f1d77ea
(no author) <(no author)@028c0714-f59e-4c53-96ac-4b4ac9e3dc78>
2007-06-12 15:22:24 +0000

    renamed setup.php to create-tables.php; moved tables creation SQL into config.php; added destroy-tables.php script;
    git-svn-id: http://svn.hidayahonline.org/repos/play@33 028c0714-f59e-4c53-96ac-4b4ac9e3dc78

--- entity/config.php
+++ entity/config.php
@@ -7,3 +7,32 @@ require_once 'lib/TypeTable.php';
 require_once 'lib/EntityTypeTable.php';
 require_once 'lib/EntityTypeEntityTypeTable.php';
 require_once 'lib/TypeTypeTable.php';
+
+$tables_sql = array();
+
+$tables_sql['entity'] = "CREATE TABLE IF NOT EXISTS `entity` (
+`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(255) NOT NULL,
+`value` TEXT NOT NULL,
+`active` ENUM('0','1') NOT NULL
+)";
+
+$tables_sql['type'] = "CREATE TABLE IF NOT EXISTS `type` (
+`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`entity_id` INT UNSIGNED NOT NULL DEFAULT '0',
+`active` ENUM('0','1') NOT NULL
+)";
+
+$tables_sql['entity_type'] = "CREATE TABLE IF NOT EXISTS `entity_type` (
+`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`entity_id` INT UNSIGNED NOT NULL DEFAULT '0',
+`type_id` INT UNSIGNED NOT NULL DEFAULT '0',
+`active` ENUM('0','1') NOT NULL
+)";
+
+$tables_sql['entity_entity'] = "CREATE TABLE IF NOT EXISTS `entity_entity` (
+`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`entity_id_1` INT UNSIGNED NOT NULL DEFAULT '0',
+`entity_id_2` INT UNSIGNED NOT NULL DEFAULT '0',
+`active` ENUM('0','1') NOT NULL
+)";
---
+++ entity/create-tables.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'config.php';
+
+if (null !== $tables_sql) {
+	foreach ($tables_sql as $table_name => $table_sql) {
+	    $db->query($table_sql);
+	}
+}
