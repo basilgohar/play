94bd54f9e879f0a70e45a0884eac0ee7d31b0433
(no author) <(no author)@028c0714-f59e-4c53-96ac-4b4ac9e3dc78>
2007-07-11 21:06:30 +0000

    Fixed logic of the construct.php script; Moved for loops to while loops;
    git-svn-id: http://svn.hidayahonline.org/repos/play@47 028c0714-f59e-4c53-96ac-4b4ac9e3dc78

--- location/construct.php
+++ location/construct.php
@@ -1,11 +1,12 @@
 <?php
 
-define('SIDE', 180);
+define('SIDE', 100);
 
 require_once 'config.inc.php';
 
 $count = 0;
 $queries = 0;
+$total = 0;
 
 $db->query('TRUNCATE TABLE `Cell`');
 
@@ -18,32 +19,49 @@ $celltype_ids_count = count($celltype_ids);
 $sql = '';
 $db->query('ALTER TABLE `Cell` DISABLE KEYS');
 
-for ($z = 1; $z <= SIDE; $z++) {
-    for ($y = 1; $y <= SIDE; $y++) {
-        for ($x = 1; $x <= SIDE; $x++) {
-            $celltype_id = $celltype_ids[mt_rand(0, $celltype_ids_count - 1)];
-            if ('' === $sql) {
-                $sql = "INSERT INTO `Cell` (`type_id`,`coord_x`,`coord_y`,`coord_z`) VALUES ($celltype_id,$x,$y,$z)";
-                $count++;
-            } else {
-                if (strlen($sql) > $max_sql_string_length) {
-                    $db->query($sql);
-                    $sql = '';
-                    $queries++;
-                } else {
-                    $sql .= ",($celltype_id,$x,$y,$z)";
-                    $count++;
-                }
+//  Since they are both data as well as counter variables, we offset them by 1
+$z = $y = $x = 0;
+
+while ($z < SIDE) {
+    while ($y < SIDE) {
+        while ($x < SIDE) {
+            if (strlen($sql) > $max_sql_string_length) {
+                //  The SQL string is getting too long, so flush the query
+                $db->query($sql);
+                $sql = '';
+                $queries++;
             }
+            
+            //  Fetch a random celltype
+            $celltype_id = $celltype_ids[mt_rand(0, $celltype_ids_count - 1)];
+                        
+            if ('' === $sql) {
+                //  We are either starting or we've just flushed the SQL string
+                $sql = "INSERT INTO `Cell` (`type_id`,`coord_x`,`coord_y`,`coord_z`) VALUES ($celltype_id,$x,$y,$z)";
+            } else {
+                //  Simply append a new set of values to the end of the SQL string
+                $sql .= ",($celltype_id,$x,$y,$z)";
+            }
+            $count++;
+            ++$x;
         }
+        $x = 0;
+        ++$y;
     }
+    $y = 0;
+    ++$z;
 }
+$z = 0;
+
 if (strlen($sql) > 0) {
+    //  Flush the remain SQL string
     $db->query($sql);
     $sql = '';
     $queries++;
 }
+
 $db->query('ALTER TABLE `Cell` ENABLE KEYS');
 $total_time = microtime(true) - $start_time;
 
 echo 'Finished ' . $count . ' iterations in ' . $total_time . ' seconds with ' . $queries . ' queries (' . ($count/$total_time)  . ' iterations per second)' . "\n";
+echo $total;
