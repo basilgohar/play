5490399ed1794938b3134c2c14f139fa3f9cd536
(no author) <(no author)@028c0714-f59e-4c53-96ac-4b4ac9e3dc78>
2007-07-23 18:17:36 +0000

    defined a new structure for local vs. global vs. default configuration scripts - stage 2
    git-svn-id: http://svn.hidayahonline.org/repos/play@52 028c0714-f59e-4c53-96ac-4b4ac9e3dc78

---
+++ village/config.local.default.php
@@ -0,0 +1,19 @@
+<?php
+
+define('VILLAGE_POPULATION', 20);
+define('VILLAGE_DISPLAY_LIMIT', 1000);
+define('VILLAGE_CONTROLLER_PATH', '/home/basil.gohar/public_html/play/village/controllers');
+define('VILLAGE_SPOUSE_MAX_MALE', 4);
+define('VILLAGE_SPOUSE_MAX_FEMALE', 1);
+define('VILLAGE_START_DATE', date('Y-m-d H:i:s'));
+
+define('VILLAGE_HEARTBEAT_PEOPLE_COUNT', 50);
+//  Possible actions for the heart beat
+define('VILLAGE_HEARTBEAT_MARRIAGE', 1);
+define('VILLAGE_HEARTBEAT_CHILD_BIRTH', 2);
+define('VILLAGE_HEARTBEAT_MURDER', 3);
+define('VILLAGE_HEARTBEAT_NATURAL_DEATH', 4);
+
+$CFG = array();
+
+$CFG['db']['dbname'] = 'play';
