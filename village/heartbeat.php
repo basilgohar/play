db3018729d34941604b2fb22f31b4745f5236017
(no author) <(no author)@028c0714-f59e-4c53-96ac-4b4ac9e3dc78>
2007-09-05 20:40:05 +0000

    made heartbeat.php less deadly
    git-svn-id: http://svn.hidayahonline.org/repos/play@58 028c0714-f59e-4c53-96ac-4b4ac9e3dc78

--- village/heartbeat.php
+++ village/heartbeat.php
@@ -3,7 +3,8 @@
 require_once 'config.php';
 
 $current_date = $db->fetchOne("SELECT `value` FROM `Info` WHERE `key` = 'current_date'");
-
+echo 'Date: ' . $current_date;
+/*
 $doctype = DOMImplementation::createDocumentType('html', '-//W3C//DTD XHTML 1.0 Strict//EN', 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd');
 
 $doc = DOMImplementation::createDocument(null, 'html', $doctype);
@@ -27,9 +28,10 @@ $meta->setAttribute('http-equiv', 'refresh');
 $meta->setAttribute('content', '0');
 
 $html->appendChild($body = $doc->createElement('body'));
+*/
 
 //  Fetch a random sampling of living people
-$people_ids = $db->fetchCol("SELECT `id` FROM `People` WHERE `date_death` = '0000-00-00 00:00:00'");
+$people_ids = $db->fetchCol("SELECT `id` FROM `People`");
 shuffle($people_ids);
 $random_people_ids = array_slice($people_ids, 0, VILLAGE_HEARTBEAT_PEOPLE_COUNT);
 unset($people_ids);
@@ -42,19 +44,19 @@ while (count($random_people_data) > 0) {
 	        'male' === $person->gender ? $spouse_gender = 'female' : $spouse_gender = 'male';
 	        if ($potential_spouse = $people->fetchRandomPersonEligableForMarriage($spouse_gender)) {
 		        if ($person->marryTo($potential_spouse)) {
-		            ///*
+                            /*
 		            $body->appendChild($p = $doc->createElement('p', $person . ' has married ' . $potential_spouse));
 		            $p->setAttribute('class','marriage');
-					//*/
+                            */
 		        }
 	        }
 	        break;
 	    case VILLAGE_HEARTBEAT_CHILD_BIRTH:
 	        if ($child = $person->haveChild()) {
-	            ///*
+	            /*
 	            $body->appendChild($p = $doc->createElement('p', $person . ' has given birth to ' . $child));
 	            $p->setAttribute('class','birth');
-	            //*/
+	            */
 	        }
 	        break;
 	        /*
@@ -85,6 +87,12 @@ $db->update('Info', array('value' => date('Y-m-d H:i:s', (strtotime($current_dat
 
 $total_time = microtime(true) - $start_time;
 
+$time_completed_string = 'Rendered page in ' . $total_time . ' seconds';
+
+/*
 $body->appendChild($doc->createElement('p', 'Rendered page in ' . $total_time . ' seconds'));
 
 echo $doc->saveXML();
+*/
+
+echo ' ' . $time_completed_string . "\n";
