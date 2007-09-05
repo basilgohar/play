43b7afd3646d21f3aad08b2c1d2cc3dccd845bff
(no author) <(no author)@028c0714-f59e-4c53-96ac-4b4ac9e3dc78>
2007-09-05 20:40:17 +0000

    removed some commented code
    git-svn-id: http://svn.hidayahonline.org/repos/play@59 028c0714-f59e-4c53-96ac-4b4ac9e3dc78

--- village/generate-people.php
+++ village/generate-people.php
@@ -22,7 +22,6 @@ $db->query('TRUNCATE TABLE `People`');
 $db->query('ALTER TABLE `People` DISABLE KEYS');
 
 $sql = '';
-//$sql_array = array();
 for ($i = 0; $i < VILLAGE_POPULATION; ++$i) {
     0 === mt_rand(0,1) ? $gender = 'male' : $gender = 'female';
     'male' === $gender ? $name_first_id = $name_ids_male[mt_rand(0, $names_male_count - 1)] : $name_first_id = $name_ids_female[mt_rand(0, $names_female_count - 1)];
@@ -35,7 +34,6 @@ for ($i = 0; $i < VILLAGE_POPULATION; ++$i) {
 	$sql .= implode(',', $person_array);
 	$sql .= '),';
 	if (strlen($sql) > $max_sql_string_length) {
-		//$sql_array[] = substr($sql, 0, -1);
 		$sql = substr($sql, 0, -1);
 		$db->query($sql);
 		$sql = '';
@@ -44,16 +42,10 @@ for ($i = 0; $i < VILLAGE_POPULATION; ++$i) {
 
 if ('' !== $sql) {
 	//  Process the last remaining SQL string
-	//$sql_array[] = substr($sql, 0, -1);
 	$sql = substr($sql, 0, -1);
 	$db->query($sql);	
 	$sql = '';
 }
-/*
-foreach ($sql_array as $sql) {
-	$db->query($sql);
-}
-*/
 $db->query('ALTER TABLE `People` ENABLE KEYS');
 
 $total_time = microtime(true) - $start_time;
